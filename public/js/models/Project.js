e9fc3ad6db543a88a214bbafe7f2385c54e6983d
Daniela Egli <eglida03@students.zhaw.ch>
2018-10-29 21:54:12 +0100

    Erstellung Tags Project

---
+++ public/js/models/Project.js
@@ -0,0 +1,16 @@
+class Project {
+    constructor(title) {
+        this.identifier = create_UUID().substr(0,4);
+        console.log("asdfasfsdfasdf");
+        this.title = title;
+    }
+/*
+    check() {
+        this.checked = true;
+    }
+
+    uncheck() {
+        this.checked = false;
+    }
+ */
+}
